49ec70438a698fec2565273707031e33077054ab
utkarshofficial <utkarshofficialin@gmail.com>
2022-08-31 13:56:01 +0530

    fixed message wrap

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { auth } from "../firebase";
 
 const style = {
-  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-[25px] rounded-tr-[25px] max-w-[70%]`,
+  message: `flex items-center shadow-xl m-1 py-2 px-3 rounded-tl-[18px] rounded-tr-[18px] max-w-[85%]`,
   name: `absolute mt-[-4rem] text-gray-600 text-xs`,
-  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-[25px]`,
-  received: `bg-[#e5e5ea] text-black float-left rounded-br-[25px]`,
+  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-[18px]`,
+  received: `bg-[#e5e5ea] text-black float-left text-start rounded-br-[18px]`,
 };
 
 const Message = ({ message }) => {
@@ -16,8 +16,12 @@ const Message = ({ message }) => {
 
   return (
     <div>
-      <div className={`${style.message} ${messageClass}`}>
-        <p className={style.name}>{message.name}</p>
+      <div
+        style={{ overflowWrap: "anywhere" }}
+        className={`${style.message} ${messageClass}`}
+      >
+        {/* showing name */}
+        {/* <p className={style.name}>{message.name}</p> */}
         <p>{message.text}</p>
       </div>
     </div>
