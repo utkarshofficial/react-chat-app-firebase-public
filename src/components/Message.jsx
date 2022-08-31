d578168a4df0f2a24873f5cef943685e6bfd8121
utkarshofficial <utkarshofficialin@gmail.com>
2022-08-31 13:01:56 +0530

    added maxwidth to message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { auth } from "../firebase";
 
 const style = {
-  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
+  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-[25px] rounded-tr-[25px] max-w-[70%]`,
   name: `absolute mt-[-4rem] text-gray-600 text-xs`,
-  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
-  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
+  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-[25px]`,
+  received: `bg-[#e5e5ea] text-black float-left rounded-br-[25px]`,
 };
 
 const Message = ({ message }) => {
